848f046d77da3776e0da9d11deb386037b81d6c6
same871 <samueltakudzwamuto@gmail.com>
2023-05-31 09:49:12 +0200

    initial commit

---
+++ components/HomePage.js
@@ -0,0 +1,27 @@
+import { useMoralis } from "react-moralis"
+import Header from "./Header"
+import LotteryEntrance from "./LotteryEntrance"
+const supportedChains = ["31337", "11155111"]
+
+export default function HomePage() {
+    const { isWeb3Enabled, chainId } = useMoralis()
+
+    return (
+        <div>
+            <Header />
+            {isWeb3Enabled ? (
+                <div>
+                    {supportedChains.includes(parseInt(chainId).toString()) ? (
+                        <div className="flex flex-row">
+                            <LotteryEntrance className="p-8" />
+                        </div>
+                    ) : (
+                        <div>{`Please switch to a supported chainId. The supported Chain Ids are: ${supportedChains}`}</div>
+                    )}
+                </div>
+            ) : (
+                <div>Please connect to a Wallet</div>
+            )}
+        </div>
+    )
+}
---
+++ components/LotteryEntrance.js
@@ -0,0 +1,113 @@
+import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddress } from "@/constants"
+import { useMoralis } from "react-moralis"
+import { useEffect, useState } from "react"
+import { ethers } from "ethers"
+import { useNotification } from "web3uikit"
+
+export default function LotteryEntrance() {
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const lotteryAddress = chainId in contractAddress ? contractAddress[chainId][0] : null
+
+    const dispatch = useNotification()
+
+    const [entranceFee, setEntranceFee] = useState("0")
+    const [numberOfPlayers, setNumberOfPlayers] = useState("0")
+    const [winner, setRecentWinner] = useState("0x")
+
+    const {
+        runContractFunction: enterLottery,
+        data: enterTxResponse,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
+        abi: abi,
+        contractAddress: lotteryAddress,
+        functionName: "enterLottery",
+        params: {},
+        msgValue: entranceFee,
+    })
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: lotteryAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
+        abi: abi,
+        contractAddress: lotteryAddress,
+        functionName: "getNumberOfPlayers",
+        params: {},
+    })
+
+    const { runContractFunction: getRecentWinner } = useWeb3Contract({
+        abi: abi,
+        contractAddress: lotteryAddress,
+        functionName: "getRecentWinner",
+        params: {},
+    })
+
+    const updateUI = async () => {
+        const entranceFeeFromCall = (await getEntranceFee()).toString()
+        const playerNumFromCall = (await getNumberOfPlayers()).toString()
+        const winnerFromCall = await getRecentWinner()
+        setEntranceFee(entranceFeeFromCall)
+        setNumberOfPlayers(playerNumFromCall)
+        setRecentWinner(winnerFromCall)
+    }
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    const handleNewNotification = () => {
+        dispatch({
+            type: "info",
+            message: "Transaction complete",
+            title: "Tx Notification",
+            position: "topR",
+        })
+    }
+
+    const handleSuccess = async (tx) => {
+        await tx.wait(1)
+        handleNewNotification(tx)
+        updateUI()
+    }
+    return (
+        <div>
+            <h1 className="py-4 px-4 font-bold text-3xl">Lottery</h1>
+            {lotteryAddress ? (
+                <>
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+                        onClick={async () => {
+                            await enterLottery({
+                                onSuccess: handleSuccess,
+                                onError: (error) => console.log(error),
+                            })
+                        }}
+                        disabled={isLoading || isFetching}
+                    >
+                        {isLoading || isFetching ? (
+                            <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                        ) : (
+                            "Enter Raffle"
+                        )}
+                    </button>
+
+                    <div> Number of players: {numberOfPlayers}</div>
+                    <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
+                    <div>Recent Winner: {winner}</div>
+                </>
+            ) : (
+                <div>Connect to supported chains</div>
+            )}
+        </div>
+    )
+}
